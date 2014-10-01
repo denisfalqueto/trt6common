b62bbe005d8c9bb2296f11576b8266551aa83506
Denis Falqueto <denisfalqueto@gmail.com>
2014-10-01 15:55:04 -0300

    Refatoração das fachadas
    A interface CrudFacade é a única que pode ser associada a uma entidade, pois o caso de uso CRUD
    justifica essa ligação. Ela expõe os métodos das interfaces CrudRepository e EntityRepository. A
    implementação busca uma instância de CrudRepository com os parâmetros necessários e
    direciona todas as chamadas para esta instância.
    
    É necessário testar se o CDI vai retornar a instância correta, pois é baseada em generics e pode
    ser que não funcione como esperado.

--- common-core/src/main/java/br/jus/trt/lib/common_core/business/facade/CrudFacade.java
+++ common-core/src/main/java/br/jus/trt/lib/common_core/business/facade/CrudFacade.java
@@ -1,39 +1,232 @@
 package br.jus.trt.lib.common_core.business.facade;
 
-import br.jus.trt.lib.common_core.integration.persistence.CrudBObject;
 import br.jus.trt.lib.common_core.business.domain.Entity;
+import br.jus.trt.lib.common_core.integration.persistence.NonUniqueEntityException;
+import br.jus.trt.lib.qbe.api.Filter;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.metamodel.SingularAttribute;
 
-public interface CrudFacade <ENTITY extends Entity<?>> extends QuerierFacade<ENTITY>{
+public interface CrudFacade<E extends Entity<PK>, PK extends Serializable> {
 
-	/**
-	 * Método responsável por excluir uma entidade.
-	 * @param entity Entidade que será excluída.
-	 */
-	public void delete(ENTITY entity);
-	
-	/**
-	 * Exclui uma entidade da base de dados.
-	 * @param id Identificador da entidade a ser excluída.
-	 */
-	public void delete(Object id);
-	
-	/**
-	 * Atualiza os dados de uma entidade na base de dados.
-	 * @param entity Objeto com dados atualizados.
-	 */
-	public void update(ENTITY entity);
-	
-	/**
-	 * Insere uma entidade na base de dados.
-	 * @param entity Entidade que será inserida.
-	 */
-	public void insert(ENTITY entity);
+    /*
+     * Redefinir métodos das interfaces herdadas. O objetivo é isolar a implementação real
+     * dos pontos de uso acima da camada de facade.
+     *
+     * A primeira parte são os métodos da interface EntityRepository. A segunda parte é
+     * de CrudRepository.
+     */
+    /**
+     * Persist (new entity) or merge the given entity. The distinction on
+     * calling either method is done based on the primary key field being null
+     * or not. If this results in wrong behavior for a specific case, consider
+     * using the {@link org.apache.deltaspike.data.api.EntityManagerDelegate}
+     * which offers both {@code persist} and {@code merge}.
+     *
+     * @param entity Entity to save.
+     * @return Returns the modified entity.
+     */
+    E save(E entity);
 
-	/**
-	 * Insere ou atualiza uma entidade na base de dados. O tipo de operação a ser realizada será verificada
-	 * no momento da execução através do método {@link CrudBObject#isInsertion(Entity)}.
-	 * @param entity Entidade que será excluída.
-	 */
-	public void save(ENTITY entity);
-	
+    /**
+     * {@link #save(Object)}s the given entity and flushes the persistence
+     * context afterwards.
+     *
+     * @param entity Entity to save.
+     * @return Returns the modified entity.
+     */
+    E saveAndFlush(E entity);
+
+    /**
+     * {@link #save(Object)}s the given entity and flushes the persistence
+     * context afterwards, followed by a refresh (e.g. to load DB trigger
+     * modifications).
+     *
+     * @param entity Entity to save.
+     * @return Returns the modified entity.
+     */
+    E saveAndFlushAndRefresh(E entity);
+
+    /**
+     * Convenience access to
+     * {@link javax.persistence.EntityManager#remove(Object)}.
+     *
+     * @param entity Entity to remove.
+     */
+    void remove(E entity);
+
+    /**
+     * Convenience access to
+     * {@link javax.persistence.EntityManager#remove(Object)} with a following
+     * flush.
+     *
+     * @param entity Entity to remove.
+     */
+    void removeAndFlush(E entity);
+
+    /**
+     * Convenience access to
+     * {@link javax.persistence.EntityManager#refresh(Object)}.
+     *
+     * @param entity Entity to refresh.
+     */
+    void refresh(E entity);
+
+    /**
+     * Convenience access to {@link javax.persistence.EntityManager#flush()}.
+     */
+    void flush();
+
+    /**
+     * Entity lookup by primary key. Convenicence method around
+     * {@link javax.persistence.EntityManager#find(Class, Object)}.
+     *
+     * @param primaryKey DB primary key.
+     * @return Entity identified by primary or null if it does not exist.
+     */
+    E findBy(PK primaryKey);
+
+    /**
+     * Lookup all existing entities of entity class {@code <E extends Entity>}.
+     *
+     * @return List of entities, empty if none found.
+     */
+    List<E> findAll();
+
+    /**
+     * Lookup a range of existing entities of entity class {@code <E extends Entity>} with
+     * support for pagination.
+     *
+     * @param start The starting position.
+     * @param max The maximum number of results to return
+     * @return List of entities, empty if none found.
+     */
+    List<E> findAll(int start, int max);
+
+    /**
+     * Query by example - for a given object and a specific set of properties.
+     *
+     * @param example Sample entity. Query all like.
+     * @param attributes Which attributes to consider for the query.
+     * @return List of entities matching the example, or empty if none found.
+     */
+    List<E> findBy(E example, SingularAttribute<E, ?>... attributes);
+
+    /**
+     * Query by example - for a given object and a specific set of properties
+     * with support for pagination.
+     *
+     * @param example Sample entity. Query all like.
+     * @param start The starting position.
+     * @param max The maximum number of results to return
+     * @param attributes Which attributes to consider for the query.
+     * @return List of entities matching the example, or empty if none found.
+     */
+    List<E> findBy(E example, int start, int max, SingularAttribute<E, ?>... attributes);
+
+    /**
+     * Query by example - for a given object and a specific set of properties
+     * using a like operator for Strings.
+     *
+     * @param example Sample entity. Query all like.
+     * @param attributes Which attributes to consider for the query.
+     * @return List of entities matching the example, or empty if none found.
+     */
+    List<E> findByLike(E example, SingularAttribute<E, ?>... attributes);
+
+    /**
+     * Query by example - for a given object and a specific set of properties
+     * using a like operator for Strings with support for pagination.
+     *
+     * @param example Sample entity. Query all like.
+     * @param start The starting position.
+     * @param max The maximum number of results to return
+     * @param attributes Which attributes to consider for the query.
+     * @return List of entities matching the example, or empty if none found.
+     */
+    List<E> findByLike(E example, int start, int max, SingularAttribute<E, ?>... attributes);
+
+    /**
+     * Count all existing entities of entity class {@code <E extends Entity>}.
+     *
+     * @return Counter.
+     */
+    Long count();
+
+    /**
+     * Count existing entities of entity class {@code <E extends Entity>} with for a given
+     * object and a specific set of properties..
+     *
+     * @param example Sample entity. Query all like.
+     * @param attributes Which attributes to consider for the query.
+     *
+     * @return Counter.
+     */
+    Long count(E example, SingularAttribute<E, ?>... attributes);
+
+    /**
+     * Count existing entities of entity class using the like operator for
+     * String attributes {@code <E extends Entity>} with for a given object and a specific set
+     * of properties..
+     *
+     * @param example Sample entity. Query all like.
+     * @param attributes Which attributes to consider for the query.
+     *
+     * @return Counter.
+     */
+    Long countLike(E example, SingularAttribute<E, ?>... attributes);
+
+    /**
+     * Busca uma entidade por ID, permitindo provocar o pré-carregamento
+     * otimizado de associações relacionadas (fetch).
+     *
+     * @param id Identificador da entidade para busca.
+     * @param fetch Lista de atributos para pré-carregamento.
+     * @return A entidade identificada pelo id informado, ou null caso não seja
+     * encontrada.
+     */
+    public E findBy(PK id, String... fetch);
+
+    /**
+     * Realiza uma consulta com filtro, retornando apenas um elemento. O filtro
+     * utilizado deverá garantir que a consulta resultará em apenas um registro,
+     * caso mais de um registro seja encontrado uma exceção será lançada.
+     *
+     * @param filter Classe que encapsula os parâmetros de consulta.
+     * @return Elemento resultante da consulta, ou null caso não encontre.
+     * @throws NonUniqueEntityException Exceção lançada quando a consulta
+     * retorna mais de um objeto.
+     */
+    public E findBy(Filter<? extends E> filter) throws NonUniqueEntityException;
+
+    /**
+     * Lista todos os registros da entidade associada de forma ordenada.
+     *
+     * @param ascedant true para listagem ascendente, false para listagem
+     * descendente
+     *
+     * @param orderBy Lista de campos que serão utilizados na ordenação. A ordem
+     * dos parametros indicará a prioridade da ordenação.
+     *
+     * @return Lista de registros ordenados.
+     */
+    public abstract List<E> findAll(boolean ascedant, String... orderBy);
+
+    /**
+     * Conta a quantidade de registros da entidade.
+     *
+     * @param filter Encapsula os parâmetros e configurações das restrições da
+     * consulta.
+     * @return a quantidade de registros da entidade.
+     */
+    public Long count(Filter<? extends E> filter);
+
+    /**
+     * Realiza uma consulta baseada nas configurações do filtro.
+     *
+     * @param filter Encapsula os parâmetros e configurações das restrições da
+     * consulta.
+     * @return Resultado da consulta.
+     */
+    public List<E> findAllBy(Filter<? extends E> filter);
 }
--- common-core/src/main/java/br/jus/trt/lib/common_core/business/facade/CrudFacadeBase.java
+++ common-core/src/main/java/br/jus/trt/lib/common_core/business/facade/CrudFacadeBase.java
@@ -1,37 +1,149 @@
 package br.jus.trt.lib.common_core.business.facade;
 
-import br.jus.trt.lib.common_core.integration.persistence.CrudBObject;
 import br.jus.trt.lib.common_core.business.domain.Entity;
+import br.jus.trt.lib.common_core.integration.persistence.CrudRepository;
+import br.jus.trt.lib.common_core.util.JavaGenericsUtil;
+import br.jus.trt.lib.qbe.api.Filter;
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.metamodel.SingularAttribute;
+import org.apache.deltaspike.core.api.provider.BeanProvider;
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
 
-@SuppressWarnings("serial")
-public abstract class CrudFacadeBase <ENTITY extends Entity<?>> 
-			extends QuerierFacadeBase<ENTITY> implements CrudFacade<ENTITY> {
+@Transactional
+@BusinessExceptionHandler
+public abstract class CrudFacadeBase<E extends Entity<PK>, PK extends Serializable>
+        implements CrudFacade<E, PK> {
 
-	@Override
-	public void delete(ENTITY entity) {
-		getBObject().delete(entity);
-	}
+    private Class<CrudRepository<E, PK>> repositoryClass;
+    private CrudRepository<E, PK> repository;
+    @Inject
+    private BeanProvider beanProvider;
 
-	@Override
-	public void delete(Object id) {
-		getBObject().delete(id);
-	}
+    protected Class<CrudRepository<E, PK>> getRepositoryClass() {
+        if (repositoryClass == null) {
+            List<Class<?>> typeArguments = JavaGenericsUtil.getGenericTypedArguments(CrudFacadeBase.class, this.getClass());
+            this.repositoryClass = (Class<CrudRepository<E, PK>>) typeArguments.get(0);
+        }
+        return this.repositoryClass;
+    }
 
-	@Override
-	public void update(ENTITY entity) {
-		getBObject().update(entity);
-	}
+    protected CrudRepository<E, PK> getRepository() {
+        if (repository == null) {
+            repository = (CrudRepository<E, PK>) beanProvider.getContextualReference(getRepositoryClass());
+        }
+        return repository;
+    }
 
-	@Override
-	public void insert(ENTITY entity) {
-		getBObject().insert(entity);
-	}
+    @Override
+    public E save(E entity) {
+        return getRepository().save(entity);
+    }
 
-	@Override
-	public void save(ENTITY entity) {
-		getBObject().save(entity);
-	}
+    @Override
+    public E saveAndFlush(E entity) {
+        return getRepository().saveAndFlush(entity);
+    }
 
-	@Override
-	protected abstract CrudBObject<ENTITY> getBObject();
+    @Override
+    public E saveAndFlushAndRefresh(E entity) {
+        return getRepository().saveAndFlushAndRefresh(entity);
+    }
+
+    @Override
+    public void remove(E entity) {
+        getRepository().remove(entity);
+    }
+
+    @Override
+    public void removeAndFlush(E entity) {
+        getRepository().removeAndFlush(entity);
+    }
+
+    @Override
+    public void refresh(E entity) {
+        getRepository().refresh(entity);
+    }
+
+    @Override
+    public void flush() {
+        getRepository().flush();
+    }
+
+    @Override
+    public E findBy(PK primaryKey) {
+        return getRepository().findBy(primaryKey);
+    }
+
+    @Override
+    public List<E> findAll() {
+        return getRepository().findAll();
+    }
+
+    @Override
+    public List<E> findAll(int start, int max) {
+        return getRepository().findAll(start, max);
+    }
+
+    @Override
+    public List<E> findBy(E example, SingularAttribute<E, ?>... attributes) {
+        return getRepository().findBy(example, attributes);
+    }
+
+    @Override
+    public List<E> findBy(E example, int start, int max, SingularAttribute<E, ?>... attributes) {
+        return getRepository().findBy(example, start, max, attributes);
+    }
+
+    @Override
+    public List<E> findByLike(E example, SingularAttribute<E, ?>... attributes) {
+        return getRepository().findByLike(example, attributes);
+    }
+
+    @Override
+    public List<E> findByLike(E example, int start, int max, SingularAttribute<E, ?>... attributes) {
+        return getRepository().findByLike(example, start, max, attributes);
+    }
+
+    @Override
+    public Long count() {
+        return getRepository().count();
+    }
+
+    @Override
+    public Long count(E example, SingularAttribute<E, ?>... attributes) {
+        return getRepository().count(example, attributes);
+    }
+
+    @Override
+    public Long countLike(E example, SingularAttribute<E, ?>... attributes) {
+        return getRepository().countLike(example, attributes);
+    }
+
+    @Override
+    public E findBy(PK id, String... fetch) {
+        return getRepository().findBy(id, fetch);
+    }
+
+    @Override
+    public E findBy(Filter<? extends E> filter) {
+        return getRepository().findBy(filter);
+    }
+
+    @Override
+    public List<E> findAll(boolean ascedant, String... orderBy) {
+        return getRepository().findAll(ascedant, orderBy);
+    }
+
+    @Override
+    public Long count(Filter<? extends E> filter) {
+        return getRepository().count(filter);
+    }
+
+    @Override
+    public List<E> findAllBy(Filter<? extends E> filter) {
+        return getRepository().findAllBy(filter);
+    }
+
 }
