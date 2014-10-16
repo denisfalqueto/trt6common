df96a3bf3fc9409ecf2a545604a6f9964072c78e
augusto <augusto.breno@trt6.gov.br>
2014-10-16 10:31:19 -0300

    Normatizando Lookup de beans para funcionar independente de ambiente (web, tests).
    A versão anterior buscava por um BeanManager na árvore JNDI, e era incompatível com o ambiente de testes. A versão atual faz uso do DeltaSpike como intermediário, que já resolve a normatização do uso do BeanManager independente do ambiente de execução.

--- common-core/src/main/java/br/jus/trt/lib/common_core/util/DIContainerUtil.java
+++ common-core/src/main/java/br/jus/trt/lib/common_core/util/DIContainerUtil.java
@@ -1,12 +1,7 @@
 package br.jus.trt.lib.common_core.util;
 
-import java.util.Iterator;
+import org.apache.deltaspike.core.api.provider.BeanProvider;
 
-import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 /**
  * Classe utilitária com operações relacionadas ao conteiner de injeção de
@@ -17,33 +12,13 @@ import javax.naming.NamingException;
  */
 public class DIContainerUtil {
 
-	public BeanManager getBeanManager() {
-		try {
-			InitialContext initialContext = new InitialContext();
-			return (BeanManager) initialContext.lookup("java:comp/BeanManager");
-		} catch (NamingException e) {
-			// TODO Log!
-			System.out.println("Couldn't get BeanManager through JNDI");
-			return null;
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T> T lookup(Class<T> clazz, BeanManager bm) {
-		Iterator<Bean<?>> iter = bm.getBeans(clazz).iterator();
-		if (!iter.hasNext()) {
-			throw new IllegalStateException(
-					"CDI BeanManager cannot find an instance of requested type "
-							+ clazz.getName());
-		}
-		Bean<T> bean = (Bean<T>) iter.next();
-		CreationalContext<T> ctx = bm.createCreationalContext(bean);
-		T result = (T) bm.getReference(bean, clazz, ctx);
-		return result;
-	}
-
+	/**
+	 * Realiza lookup de um Bean a partir do container de injeção de dependência ativo.
+	 * @param clazz Tipo do bean para lookup.
+	 * @return Bean instanciado e com suas dependências injetadas.
+	 */
 	public <T> T lookup(Class<T> clazz) {
-		BeanManager bm = getBeanManager();
-		return lookup(clazz, bm);
+		return BeanProvider.getContextualReference(clazz, false);
 	}
+	
 }
