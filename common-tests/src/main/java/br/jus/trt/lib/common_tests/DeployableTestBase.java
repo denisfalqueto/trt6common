229f32cc5e4e409d7c0b86d9dd7c83512481b225
Augusto Breno <augusto.breno@trt6.jus.br>
2015-01-09 09:42:09 -0300

    Alterando nome da classe para um mais adequado

---
+++ common-tests/src/main/java/br/jus/trt/lib/common_tests/DeployableTestBase.java
@@ -0,0 +1,16 @@
+package br.jus.trt.lib.common_tests;
+
+import org.junit.runner.RunWith;
+
+import br.jus.trt.lib.common_tests.arquillian.ArquillianCommonRunner;
+
+/**
+ * Teste executado em um ambiente JEE real, com todos os recursos disponíveis.
+ * Ideal para testes de integração, testes de comportamento, etc.
+ * @author augusto
+ *
+ */
+@RunWith(ArquillianCommonRunner.class)
+public class DeployableTestBase extends TestBase {
+
+}
